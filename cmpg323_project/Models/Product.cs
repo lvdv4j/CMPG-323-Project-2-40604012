3ef5b66ce10545e865a1476dfbc6061709e1102f
Lizzon <lvdv4j@gmail.com>
2023-08-27 13:15:27 +0200

    Scaffolded all the models

---
+++ cmpg323_project/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cmpg323_project.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public short ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string? ProductDescription { get; set; }
+        public int? UnitsInStock { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
